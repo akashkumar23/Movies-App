5751e0f366d6c1209a8c9889c745f6de2253b6b3
Akash Kumar <77935092+akashkumar23@users.noreply.github.com>
2023-06-18 05:24:19 +0530

    version 1.0

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                {/* <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link> */}
+                <Link to="/"><img className="header__icon" src="https://i.pinimg.com/280x280_RS/f6/ff/58/f6ff585979e83d8a5cb49b6211c69b56.jpg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
